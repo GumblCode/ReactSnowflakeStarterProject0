6569d7af239ab48f60615026139cae28d75e4dde
Rainer <126818957+GumblCode@users.noreply.github.com>
2024-06-29 11:23:23 +0200

    ReactSnowflakeStarterProject0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TestKomponenteFunction from "./TestKomponenteFunction";
+import "./index.css";
+
+function App() {
+	return (
+		<article>
+			<h1>NewReactProject0</h1>
+      <TestKomponenteFunction />
+		</article>
+	);
+}
+
+export default App;
